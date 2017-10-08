3f4a3b122c795712993e0c579e5daf7bab5129cc
Yizhen Zhao <yizhen@uber.com>
2017-10-07 20:09:08 -0700

    upgrade kiws version

--- src/account.ts
+++ src/account.ts
@@ -8,11 +8,12 @@ const ACCOUNT_PROVIDER_TAGS = [ 'account', 'provider' ];
 const WORKER_METADATA_KEY  = kiws.ENDPOINT_METADATA_KEY;
 
 export function Account(options: AccountOptions) {
-  const tags        = _.union(ACCOUNT_PROVIDER_TAGS, options.tags);
-  const meta        = _.extend({}, options.meta);
-  meta.accountType  = options.accountType;
-  meta.provider     = options.provider;
-  return kiws.Injectable(_.extend({}, options, { tags }, { meta }));
+  const tags               = _.union(ACCOUNT_PROVIDER_TAGS, options.tags);
+  const meta               = _.extend({}, options.meta);
+  meta.accountType         = options.accountType;
+  meta.provider            = options.provider;
+  const injectableOptions  = _.extend({}, options, { tags }, { meta });
+  return kiws.Injectable(injectableOptions);
 }
 
 export interface AccountOptions {
